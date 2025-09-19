4e17c06fc0642e14f49397820a3d3da500314314
James Todd <jtodd@navigate360.com>
2025-09-19 16:02:37 -0700

    Correct bug

--- scripts/kiosk-manager.js
+++ scripts/kiosk-manager.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { spawn } from 'child_process';
-import { config, loadConfig } from './src/config.js';
+import { config, loadConfig } from '../src/config.js';
 
 class KioskManager {
     constructor() {
